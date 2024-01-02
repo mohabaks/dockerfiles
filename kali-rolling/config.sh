3870b0fc63ccf8bcf174a348d32eb358de33c580
mohabaks <mohabaks@protonmail.com>
2024-01-02 09:13:33 +0300

    Updated kali-rolling

--- kali-rolling/config.sh
+++ kali-rolling/config.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-# setup XDG_RUNTIME_DIR
-if [[ ! -d /run/user/1000 ]];then
-	sudo mkdir -p  /run/user/1000
-	sudo chown ${KALI_USER}:${KALI_USER} /run/user/1000
-	sudo chmod 0700 /run/user/1000
-fi
-
-# changing permission for copied files
-sudo chown ${KALI_USER}:${KALI_USER} /home/${KALI_USER}/.zsh*
-sudo chown ${KALI_USER}:${KALI_USER} /home/${KALI_USER}/.tmux.conf
-sudo chown ${KALI_USER}:${KALI_USER} /home/${KALI_USER}/.func
+# Additional configuration goes here
 
 #sudo apt autoremove -y --purge ghidra burpsuite
